adf081fb274499cc1c286a42c9229eab7d69d049
Anonymous <Anonymous@DESKTOP-M3HB1JH>
2020-03-10 15:09:14 +0530

    java practise code within

---
+++ src/main/java/javapractisecode/practisecode/App.java
@@ -0,0 +1,13 @@
+package javapractisecode.practisecode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
